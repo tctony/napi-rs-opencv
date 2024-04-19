f99395f0f50b5c63bffdaad6e2945d25fcdca487
Tony Tang <tangchang21@gmail.com>
2024-04-19 19:04:47 +0800

    fix index.js and yarn.lock

--- index.js
+++ index.js
@@ -32,24 +32,24 @@ switch (platform) {
   case 'android':
     switch (arch) {
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, 'opencv.android-arm64.node'))
+        localFileExisted = existsSync(join(__dirname, 'napi-rs-opencv.android-arm64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./opencv.android-arm64.node')
+            nativeBinding = require('./napi-rs-opencv.android-arm64.node')
           } else {
-            nativeBinding = require('@napi-rs/opencv-android-arm64')
+            nativeBinding = require('napi-rs-opencv-android-arm64')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm':
-        localFileExisted = existsSync(join(__dirname, 'opencv.android-arm-eabi.node'))
+        localFileExisted = existsSync(join(__dirname, 'napi-rs-opencv.android-arm-eabi.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./opencv.android-arm-eabi.node')
+            nativeBinding = require('./napi-rs-opencv.android-arm-eabi.node')
           } else {
-            nativeBinding = require('@napi-rs/opencv-android-arm-eabi')
+            nativeBinding = require('napi-rs-opencv-android-arm-eabi')
           }
         } catch (e) {
           loadError = e
@@ -63,13 +63,13 @@ switch (platform) {
     switch (arch) {
       case 'x64':
         localFileExisted = existsSync(
-          join(__dirname, 'opencv.win32-x64-msvc.node')
+          join(__dirname, 'napi-rs-opencv.win32-x64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./opencv.win32-x64-msvc.node')
+            nativeBinding = require('./napi-rs-opencv.win32-x64-msvc.node')
           } else {
-            nativeBinding = require('@napi-rs/opencv-win32-x64-msvc')
+            nativeBinding = require('napi-rs-opencv-win32-x64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -77,13 +77,13 @@ switch (platform) {
         break
       case 'ia32':
         localFileExisted = existsSync(
-          join(__dirname, 'opencv.win32-ia32-msvc.node')
+          join(__dirname, 'napi-rs-opencv.win32-ia32-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./opencv.win32-ia32-msvc.node')
+            nativeBinding = require('./napi-rs-opencv.win32-ia32-msvc.node')
           } else {
-            nativeBinding = require('@napi-rs/opencv-win32-ia32-msvc')
+            nativeBinding = require('napi-rs-opencv-win32-ia32-msvc')
           }
         } catch (e) {
           loadError = e
@@ -91,13 +91,13 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'opencv.win32-arm64-msvc.node')
+          join(__dirname, 'napi-rs-opencv.win32-arm64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./opencv.win32-arm64-msvc.node')
+            nativeBinding = require('./napi-rs-opencv.win32-arm64-msvc.node')
           } else {
-            nativeBinding = require('@napi-rs/opencv-win32-arm64-msvc')
+            nativeBinding = require('napi-rs-opencv-win32-arm64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -108,23 +108,23 @@ switch (platform) {
     }
     break
   case 'darwin':
-    localFileExisted = existsSync(join(__dirname, 'opencv.darwin-universal.node'))
+    localFileExisted = existsSync(join(__dirname, 'napi-rs-opencv.darwin-universal.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./opencv.darwin-universal.node')
+        nativeBinding = require('./napi-rs-opencv.darwin-universal.node')
       } else {
-        nativeBinding = require('@napi-rs/opencv-darwin-universal')
+        nativeBinding = require('napi-rs-opencv-darwin-universal')
       }
       break
     } catch {}
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(join(__dirname, 'opencv.darwin-x64.node'))
+        localFileExisted = existsSync(join(__dirname, 'napi-rs-opencv.darwin-x64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./opencv.darwin-x64.node')
+            nativeBinding = require('./napi-rs-opencv.darwin-x64.node')
           } else {
-            nativeBinding = require('@napi-rs/opencv-darwin-x64')
+            nativeBinding = require('napi-rs-opencv-darwin-x64')
           }
         } catch (e) {
           loadError = e
@@ -132,13 +132,13 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'opencv.darwin-arm64.node')
+          join(__dirname, 'napi-rs-opencv.darwin-arm64.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./opencv.darwin-arm64.node')
+            nativeBinding = require('./napi-rs-opencv.darwin-arm64.node')
           } else {
-            nativeBinding = require('@napi-rs/opencv-darwin-arm64')
+            nativeBinding = require('napi-rs-opencv-darwin-arm64')
           }
         } catch (e) {
           loadError = e
@@ -152,12 +152,12 @@ switch (platform) {
     if (arch !== 'x64') {
       throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
     }
-    localFileExisted = existsSync(join(__dirname, 'opencv.freebsd-x64.node'))
+    localFileExisted = existsSync(join(__dirname, 'napi-rs-opencv.freebsd-x64.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./opencv.freebsd-x64.node')
+        nativeBinding = require('./napi-rs-opencv.freebsd-x64.node')
       } else {
-        nativeBinding = require('@napi-rs/opencv-freebsd-x64')
+        nativeBinding = require('napi-rs-opencv-freebsd-x64')
       }
     } catch (e) {
       loadError = e
@@ -168,26 +168,26 @@ switch (platform) {
       case 'x64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'opencv.linux-x64-musl.node')
+            join(__dirname, 'napi-rs-opencv.linux-x64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./opencv.linux-x64-musl.node')
+              nativeBinding = require('./napi-rs-opencv.linux-x64-musl.node')
             } else {
-              nativeBinding = require('@napi-rs/opencv-linux-x64-musl')
+              nativeBinding = require('napi-rs-opencv-linux-x64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'opencv.linux-x64-gnu.node')
+            join(__dirname, 'napi-rs-opencv.linux-x64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./opencv.linux-x64-gnu.node')
+              nativeBinding = require('./napi-rs-opencv.linux-x64-gnu.node')
             } else {
-              nativeBinding = require('@napi-rs/opencv-linux-x64-gnu')
+              nativeBinding = require('napi-rs-opencv-linux-x64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -197,26 +197,26 @@ switch (platform) {
       case 'arm64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'opencv.linux-arm64-musl.node')
+            join(__dirname, 'napi-rs-opencv.linux-arm64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./opencv.linux-arm64-musl.node')
+              nativeBinding = require('./napi-rs-opencv.linux-arm64-musl.node')
             } else {
-              nativeBinding = require('@napi-rs/opencv-linux-arm64-musl')
+              nativeBinding = require('napi-rs-opencv-linux-arm64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'opencv.linux-arm64-gnu.node')
+            join(__dirname, 'napi-rs-opencv.linux-arm64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./opencv.linux-arm64-gnu.node')
+              nativeBinding = require('./napi-rs-opencv.linux-arm64-gnu.node')
             } else {
-              nativeBinding = require('@napi-rs/opencv-linux-arm64-gnu')
+              nativeBinding = require('napi-rs-opencv-linux-arm64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -226,26 +226,26 @@ switch (platform) {
       case 'arm':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'opencv.linux-arm-musleabihf.node')
+            join(__dirname, 'napi-rs-opencv.linux-arm-musleabihf.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./opencv.linux-arm-musleabihf.node')
+              nativeBinding = require('./napi-rs-opencv.linux-arm-musleabihf.node')
             } else {
-              nativeBinding = require('@napi-rs/opencv-linux-arm-musleabihf')
+              nativeBinding = require('napi-rs-opencv-linux-arm-musleabihf')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'opencv.linux-arm-gnueabihf.node')
+            join(__dirname, 'napi-rs-opencv.linux-arm-gnueabihf.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./opencv.linux-arm-gnueabihf.node')
+              nativeBinding = require('./napi-rs-opencv.linux-arm-gnueabihf.node')
             } else {
-              nativeBinding = require('@napi-rs/opencv-linux-arm-gnueabihf')
+              nativeBinding = require('napi-rs-opencv-linux-arm-gnueabihf')
             }
           } catch (e) {
             loadError = e
@@ -255,26 +255,26 @@ switch (platform) {
       case 'riscv64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'opencv.linux-riscv64-musl.node')
+            join(__dirname, 'napi-rs-opencv.linux-riscv64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./opencv.linux-riscv64-musl.node')
+              nativeBinding = require('./napi-rs-opencv.linux-riscv64-musl.node')
             } else {
-              nativeBinding = require('@napi-rs/opencv-linux-riscv64-musl')
+              nativeBinding = require('napi-rs-opencv-linux-riscv64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'opencv.linux-riscv64-gnu.node')
+            join(__dirname, 'napi-rs-opencv.linux-riscv64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./opencv.linux-riscv64-gnu.node')
+              nativeBinding = require('./napi-rs-opencv.linux-riscv64-gnu.node')
             } else {
-              nativeBinding = require('@napi-rs/opencv-linux-riscv64-gnu')
+              nativeBinding = require('napi-rs-opencv-linux-riscv64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -283,13 +283,13 @@ switch (platform) {
         break
       case 's390x':
         localFileExisted = existsSync(
-          join(__dirname, 'opencv.linux-s390x-gnu.node')
+          join(__dirname, 'napi-rs-opencv.linux-s390x-gnu.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./opencv.linux-s390x-gnu.node')
+            nativeBinding = require('./napi-rs-opencv.linux-s390x-gnu.node')
           } else {
-            nativeBinding = require('@napi-rs/opencv-linux-s390x-gnu')
+            nativeBinding = require('napi-rs-opencv-linux-s390x-gnu')
           }
         } catch (e) {
           loadError = e
